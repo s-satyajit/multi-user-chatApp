a9c7d87bb6883b4e3ac3717a0ea7eab33c220ccb
Satyajit Samal <samalsatyajit668@gmail.com>
2024-12-05 19:49:45 +0530

    wip: Add chat section with the format, and put some dummy texts and images

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,100 @@
-import React from "react";
-import "./chat.css"
+import React, { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-    return (
-        <div className="chat">Chat</div>
-    )
-}
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
 
-export default Chat
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="icons">
+            <img src="./phone.png" alt="" />
+            <img src="./video.png" alt="" />
+            <img src="./info.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex facere quidem mollitia magnam culpa. Officia obcaecati repudiandae ea voluptates error porro eligendi necessitatibus rerum quibusdam quae, tempora voluptatum unde vero.</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className="message own">
+            <div className="texts">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex facere quidem mollitia magnam culpa. Officia obcaecati repudiandae ea voluptates error porro eligendi necessitatibus rerum quibusdam quae, tempora voluptatum unde vero.</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex facere quidem mollitia magnam culpa. Officia obcaecati repudiandae ea voluptates error porro eligendi necessitatibus rerum quibusdam quae, tempora voluptatum unde vero.</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className="message own">
+            <div className="texts">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex facere quidem mollitia magnam culpa. Officia obcaecati repudiandae ea voluptates error porro eligendi necessitatibus rerum quibusdam quae, tempora voluptatum unde vero.</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex facere quidem mollitia magnam culpa. Officia obcaecati repudiandae ea voluptates error porro eligendi necessitatibus rerum quibusdam quae, tempora voluptatum unde vero.</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className="message own">
+            <div className="texts">
+                <img src="https://images.pexels.com/photos/29582939/pexels-photo-29582939/free-photo-of-morning-coffee-with-biscuits-in-cozy-setting.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Example Image" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex facere quidem mollitia magnam culpa. Officia obcaecati repudiandae ea voluptates error porro eligendi necessitatibus rerum quibusdam quae, tempora voluptatum unde vero.</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
